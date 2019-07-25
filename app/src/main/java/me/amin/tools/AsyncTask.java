7239fb6bc5fee833b550dfb4745be9fc6269b913
amingoli78 <amin.goli78@gmail.com>
2019-07-25 19:19:22 +0430

    add asynctask class0

---
+++ app/src/main/java/me/amin/tools/AsyncTask.java
@@ -0,0 +1,14 @@
+package me.amin.tools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsyncTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_async_task);
+    }
+}
--- app/src/main/java/me/amin/tools/MainActivity.java
+++ app/src/main/java/me/amin/tools/MainActivity.java
@@ -2,7 +2,9 @@ package me.amin.tools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view){
+        String viewTag = (String) view.getTag();
+
+        switch (viewTag){
+            case "asyncTask":
+                startActivity(new Intent(this,));
+                break;
+
+            default: break;
+        }
+
+    }
+
 }
